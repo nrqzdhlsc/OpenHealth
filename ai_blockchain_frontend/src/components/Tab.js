1fa58b1d79e697c93d150e2219c9058e5705693a
nrqzdhlsc <zlxtqbdnrqzdhlsc@gmail.com>
2019-07-19 15:07:31 +0800

    modify frontend

--- ai_blockchain_frontend/src/components/Tab.js
+++ ai_blockchain_frontend/src/components/Tab.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tab } from 'semantic-ui-react';
 
 const panes = [
-    { menuItem: '数据共享', render: () => <Tab.Pane attached={false}>数据共享简介</Tab.Pane> },
+    { menuItem: '数据共享', render: () => <Tab.Pane ui centered ={false}>数据共享简介</Tab.Pane> },
     { menuItem: '联邦学习', render: () => <Tab.Pane attached={false}>联邦学习简介</Tab.Pane> },
     { menuItem: 'AI市场', render: () => <Tab.Pane attached={false}>AI市场简介</Tab.Pane> },
 ]
