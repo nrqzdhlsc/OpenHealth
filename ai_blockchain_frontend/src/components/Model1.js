65c1a6e64abfea3d8e4fe219ae9ced479145a905
tygavin <tianyao@zju.edu.cn>
2019-07-20 21:27:17 +0800

    [gty] add model layout

--- ai_blockchain_frontend/src/components/Model1.js
+++ ai_blockchain_frontend/src/components/Model1.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
-
+import DropdownExampleSelection from './DropDownBox';
+import {
+    Button,
+    Container,
+    Menu,
+    Responsive,
+    Segment,
+    Visibility,
+    Image,
+    Icon,
+    Grid
+} from 'semantic-ui-react';
 
 class Model1 extends Component {
     constructor(props) {
@@ -10,6 +21,31 @@ class Model1 extends Component {
         return (
             <div>
                 <NavBar />
+                <Grid >
+                    <Grid.Row centered>
+                        <Grid.Column width={8} >
+                            <Image
+                                src='https://react.semantic-ui.com/images/wireframe/image-text.png'
+                                as='a'
+                                href='/model1'
+                                target='_blank'
+                            />
+
+                        </Grid.Column>
+                    </Grid.Row>
+
+                    <Grid.Row  centered columns={10}>
+                        <Grid.Column width={8}>
+                                <DropdownExampleSelection/>
+
+                        </Grid.Column>
+
+                        <Grid.Column>
+                                <Button primary>检测</Button>
+                        </Grid.Column>
+
+                    </Grid.Row>     
+                </Grid>
             </div>
         )
     }
