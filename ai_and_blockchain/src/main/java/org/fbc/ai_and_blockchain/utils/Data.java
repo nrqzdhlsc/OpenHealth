157e7ea0e112df8f9d82490593a986f60c81ac9c
SherLzp <xiaobai084@163.com>
2019-07-20 17:17:01 +0800

    Data Share Project and Reencryption

---
+++ ai_and_blockchain/src/main/java/org/fbc/ai_and_blockchain/utils/Data.java
@@ -0,0 +1,47 @@
+package org.fbc.ai_and_blockchain.utils;
+
+public class Data {
+    private String dataId;
+    private String owner;
+    private String relevant;
+    private String dataIndex;
+
+    public Data(String dataId, String owner, String relevant, String dataIndex) {
+        this.dataId = dataId;
+        this.owner = owner;
+        this.relevant = relevant;
+        this.dataIndex = dataIndex;
+    }
+
+    public String getDataId() {
+        return dataId;
+    }
+
+    public void setDataId(String dataId) {
+        this.dataId = dataId;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getRelevant() {
+        return relevant;
+    }
+
+    public void setRelevant(String relevant) {
+        this.relevant = relevant;
+    }
+
+    public String getDataIndex() {
+        return dataIndex;
+    }
+
+    public void setDataIndex(String dataIndex) {
+        this.dataIndex = dataIndex;
+    }
+}
---
+++ ai_and_blockchain/src/main/java/org/fbc/ai_and_blockchain/utils/User.java
@@ -0,0 +1,52 @@
+package org.fbc.ai_and_blockchain.utils;
+
+import java.math.BigInteger;
+
+public class User {
+    private String account;
+    private String pubKey;
+    private String verifyKey;
+    private BigInteger userType;
+
+    public User() {
+    }
+
+    public User(String account, String pubKey, String verifyKey, BigInteger userType) {
+        this.account = account;
+        this.pubKey = pubKey;
+        this.verifyKey = verifyKey;
+        this.userType = userType;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPubKey() {
+        return pubKey;
+    }
+
+    public void setPubKey(String pubKey) {
+        this.pubKey = pubKey;
+    }
+
+    public String getVerifyKey() {
+        return verifyKey;
+    }
+
+    public void setVerifyKey(String verifyKey) {
+        this.verifyKey = verifyKey;
+    }
+
+    public BigInteger getUserType() {
+        return userType;
+    }
+
+    public void setUserType(BigInteger userType) {
+        this.userType = userType;
+    }
+}
